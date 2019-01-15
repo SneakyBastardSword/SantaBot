54365975b5fc4496c2d55366df3ad8c2d1a58ed5
Akaash Chikarmane <avchikar97@gmail.com>
2019-01-15 14:14:59 -0800

    Not for use in the dev branch. See comments. May not be necessary due to reconnect=True.

--- run_santa.sh
+++ run_santa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### Honestly don't use this unless you KNOW it's not going to fail.
-### This script was written because discord.py does not relaunch bots that run into WebSocket errors but the rewrite does.
+### This script was written because discord.py does not relaunch bots that run into WebSocket errors but the rewrite does. Requires two keyboard interrupts to end.
 ### This is to get around that.
 
 while true
